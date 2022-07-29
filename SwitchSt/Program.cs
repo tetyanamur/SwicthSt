2013453c5b6ba51943fe981bd91f537bb81c78d6
Tetyana Murzina <tetyanamur@gmail.com>
2022-07-29 17:44:01 -0500

    initial commit

---
+++ SwitchSt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SwitchSt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            var response = Console.ReadLine();
+            switch (response)
+            {
+                case "Literature":
+                    Console.WriteLine($"Literature is for erudites");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science teaches about nature");
+                    break;
+                case "Math":
+                    Console.WriteLine("Math shows students how to read numbers");
+                    break;
+                case "Art":
+                    Console.WriteLine("Art is great to develop creativity");
+                    break;
+                case "History":
+                    Console.WriteLine("History is very informative regarding time");
+                    break;
+                default:
+                    Console.WriteLine("Awesome. This is a new subject for me to learn about");
+                    break ;
+            }
+        }
+    }
+}
